5caaab7b88705b9f3e459a4eccdec04fd6f44e3e
ajpurpora <ajpurpora@gmail.com>
2020-07-28 04:27:56 -0400

    Setup

--- 6_TurnBasedRPG/Assets/Scripts/Player/PlayerController.cs
+++ 6_TurnBasedRPG/Assets/Scripts/Player/PlayerController.cs
@@ -2,5 +2,32 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
+    
+    private Rigidbody2D myRigidbody;
+
+    [Header("Public Values")]
+    [SerializeField] private float moveSpeed = 4f;
+    public float movePlayerHorizontal;
+    public float movePlayerVertical;
+
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        myRigidbody = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
+    }
+
+    private void Update()
+    {
+        
+
+        movePlayerHorizontal = Input.GetAxisRaw("Horizontal");
+        movePlayerVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(movePlayerHorizontal, movePlayerVertical);
+
+        myRigidbody.velocity = movement * moveSpeed;
+    }
+
+    
 }
